533a528626e60a97c01c5ed5020003f45f5b9bfe
HaMinhKhoi2992000 <70828789+HaMinhKhoi2992000@users.noreply.github.com>
2023-02-05 12:26:09 +0700

    Install prettier lint-staged husky

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-const path = require('path')
-const express = require('express')
-const morgan = require('morgan')
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
 const hbs = require('express-handlebars');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+const route = require('./routes');
 
 //Static files
 app.use('/public', express.static('./src/public'));
@@ -14,26 +15,29 @@ app.use('/css', express.static('./node_modules/bootstrap/dist/css'));
 app.use('/js', express.static('./node_modules/bootstrap/dist/js'));
 
 //HTTP middleware logger
-app.use(morgan('combined'))
+app.use(morgan('combined'));
+
+//Middleware for processing json, url post method data
+app.use(
+              express.urlencoded({
+                                   extended: true,
+    }),
+);
+app.use(express.json());
 
 //Templete engine
-app.engine('hbs', hbs.engine(
-  {
-    extname: '.hbs'
-  }
-));
+app.engine(
+    'hbs',
+    hbs.engine({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
-app.set('views', './src/resource/views')
+app.set('views', './src/resource/views');
 
-
-app.get('/', (req, res) => {
-  res.render('home')
-})
-
-app.get('/news', (req, res) => {
-  res.render('news')
-})
+//Route init
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
